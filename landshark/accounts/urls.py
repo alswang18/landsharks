2fd1d05238923cbaf118da4d98883eaf9d6db31d
Alec Wang <alswang18@gmail.com>
2020-10-18 11:25:39 -0700

    added login functionality and fixing dashboard

--- landshark/accounts/urls.py
+++ landshark/accounts/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login',views.index, name='login'),
-    path('register', views.listing, name='register'),
-    path('logout', views.search, name='logout'),
-    path('logout', views.search, name='dashboard')
+    path('login',views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard')
 ]
